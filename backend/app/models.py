14964eac8e866b0491534a653e18a776c1e311cf
Yoga <nikita.grebenjuk@gmail.com>
2025-10-13 23:40:24 +0200

    Phase 1 - simple structure created

---
+++ backend/app/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+class Wallet(Base):
+    __tablename__ = "wallets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    address = Column(String, unique=True, index=True)
+    label = Column(String, nullable=True)
---
+++ backend/app/routers/wallet.py
@@ -0,0 +1,5 @@
+class Wallet(Base):
+    __tablename__ = "wallets"
+    id = Column(Integer, primary_key=True, index=True)
+    address = Column(String, unique=True, index=True)
+    label = Column(String, nullable=True)
