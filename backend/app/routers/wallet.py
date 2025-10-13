9be6e9592c7a41974148974a3b5ca53aab8a043e
Yoga <nikita.grebenjuk@gmail.com>
2025-10-14 01:14:19 +0200

    backendcontainer and db-container connected - Phase 1 completed

--- backend/app/routers/wallet.py
+++ backend/app/routers/wallet.py
@@ -1,3 +1,6 @@
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
 class Wallet(Base):
     __tablename__ = "wallets"
     id = Column(Integer, primary_key=True, index=True)
